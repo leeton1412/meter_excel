6d05f9e884516d64d719631d78f42dd53ae13e48
leeton1412 <leighton.griffith87@gmail.com>
2021-08-09 22:35:37 +0000

    added table with data button, will render all data

--- meters/urls.py
+++ meters/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import index
+from .views import index, data
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+
+    url(r'data', data, name='data'),
 ]
--- meters/views.py
+++ meters/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Meters
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def data(request):
+    items = Meters.objects.all()
+    context = {
+        'items': items,
+    }
+    
+    return render(request, 'index.html', context)
