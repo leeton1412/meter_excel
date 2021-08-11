60361f0dc8402e7952cc830377218092b3901cf9
leeton1412 <leighton.griffith87@gmail.com>
2021-08-11 18:56:21 +0000

    Import_export working

--- meters/admin.py
+++ meters/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Meters
 from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
 # Register your models here.
 
 
 @admin.register(Meters)
 class ViewAdmin(ImportExportModelAdmin):
     pass
+
+
+class MeterResource(resources.ModelResource):
+
+    class Meta:
+        model = Meters
+        fields = ('customer_name', 'customer_address', 'meter_number', 
+            'day_rate', 'night_rate', 'weekend_rate', 
+            'cost_for_day_rate', 'cost_for_night_rate', 'cost_for_weekend_rate', 
+            'total_energy_cost')
+        exclude = ('rate_name')
--- meters/models.py
+++ meters/models.py
@@ -5,13 +5,17 @@ from django.db import models
 
 class Meters(models.Model):
 
-    customer_name = models.CharField('Customer Name', max_length=100, blank=True)
+    customer_name = models.CharField(max_length=100, blank=True, null=True)
     customer_address = models.CharField(max_length=80, null=True, blank=True)
     rate_name = models.CharField(max_length=80, null=True, blank=True)
-    meter_number = models.CharField(max_length=100, blank=False)
+    meter_number = models.CharField(max_length=100, blank=False, null=True)
     day_rate = models.FloatField(max_length=100, null=True)
     night_rate = models.FloatField(null=True)
     weekend_rate = models.FloatField(null=True)
+    cost_for_day_rate = models.FloatField(null=True)
+    cost_for_night_rate = models.FloatField(null=True)
+    cost_for_weekend_rate = models.FloatField(null=True)
+    total_energy_cost = models.FloatField(null=True)
 
-    def __str__(self):
-        return self.Customer_Name
+    #def __str__(self):
+        #return self.customer_name
