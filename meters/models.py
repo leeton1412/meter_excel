27e2cfcf1c9ea6a7c67b2662bfdf56af5ec10f04
leeton1412 <leighton.griffith87@gmail.com>
2021-08-11 10:21:45 +0000

    Last commit

--- meters/models.py
+++ meters/models.py
@@ -5,13 +5,13 @@ from django.db import models
 
 class Meters(models.Model):
 
-    Customer_Name = models.CharField('Customer Name', max_length=100, blank=False)
-    Customer_Address = models.CharField(max_length=80, null=True, blank=True)
-    Rate_Name = models.CharField(max_length=80, null=True, blank=True)
-    Meter_Number = models.CharField(max_length=100, blank=False)
-    Day_Rate = models.FloatField(max_length=100, null=True)
-    Night_Rate = models.FloatField(null=True)
-    Weekend_Rate = models.FloatField(null=True)
+    customer_name = models.CharField('Customer Name', max_length=100, blank=True)
+    customer_address = models.CharField(max_length=80, null=True, blank=True)
+    rate_name = models.CharField(max_length=80, null=True, blank=True)
+    meter_number = models.CharField(max_length=100, blank=False)
+    day_rate = models.FloatField(max_length=100, null=True)
+    night_rate = models.FloatField(null=True)
+    weekend_rate = models.FloatField(null=True)
 
     def __str__(self):
         return self.Customer_Name
